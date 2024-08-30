5da25918db6c9cae284bbdecae0cc469e6f22f46
Gamer4lifeovi <ukporheo@gmail.com>
2024-08-30 16:44:44 +0100

    New Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//when the game is loded:
+//all feild should be clearded
+//player 1 start(X is player one)
+//player 2 follows
+//If player 1 aligns X vertically,horizontally or diagonally in 3(XXX) he the x should be colored red and a text Horray should show for player 1
+//If player 2 aligns o vertically,horizontally or diagonally in 3(OOO) he the x should be colored red and a text Horray should show for player 2
+//If there shoould be a draw which is that none of them are able to align either thier xxx or ooo in 3 the game should autommatically refresh and and they should start again
